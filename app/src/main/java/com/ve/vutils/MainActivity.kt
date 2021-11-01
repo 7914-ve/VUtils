86430aa9b22d24811ea01dd0dff5f041c3fbee9e
ve <791422171@qq.com>
2021-11-01 23:31:41 +0800

    first commit

---
+++ app/src/main/java/com/ve/vutils/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ve.vutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.ve.veTools.LogUtil
+
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "mySDKTest"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<View>(R.id.button) as Button
+        button.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                LogUtil.d("You clicked module button!!  1234")
+            }
+        })
+    }
+
+}
